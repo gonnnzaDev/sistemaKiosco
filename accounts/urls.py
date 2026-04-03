36a6e09257c7d7bfecca23fbeb0476ccefe434f0
gonnnzaDev <gonnnzadev@gmail.com>
2026-04-02 22:16:33 -0300

    arregle css

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from accounts.views import login_view, register_view
-
-urlpatterns = [
-    path('', login_view, name='home'),
-    path('login/', login_view, name='login'),
-    path('register/', register_view, name='register'),
-]
--- sistemaKiosco/urls.py
+++ sistemaKiosco/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from sistemaKiosco.sistemaKiosco.views import login_view, register_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
+    path('', login_view, name='home'),
+    path('login/', login_view, name='login'),
+    path('register/', register_view, name='register'),
 ]
